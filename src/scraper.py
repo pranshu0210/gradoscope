1f562035f15c4f34a0dbce43a68d3dd15879291a
Pranshu Dave <pranshudave@gmail.com>
2019-03-09 18:42:00 +0530

    Add proper scraping and notifying functionality

--- src/scraper.py
+++ src/scraper.py
@@ -1,16 +1,79 @@
-import requests
-import urllib.request
 import time
+
+import requests
 from bs4 import BeautifulSoup
+from notify_run import Notify
 
-url = 'https://www.thegradcafe.com/survey/index.php?q=Brown+University+computer+science&t=a&o=&p=1'
+notify = Notify()
+universities = [['Brown', 'University'], ['Purdue', 'University'], ['Simon', 'Fraser'], ['Mcgill'], ['Toronto'],
+                ['British', 'Columbia'], ['Alberta'], ['Georgia', 'Institute'], ['Waterloo']]
 
-response = requests.get(url)
+# universities = [['Brown', 'University']]
 
-soup = BeautifulSoup(response.text, "html.parser")
+urls = {}
+for uni in universities:
+    u_str = ""
+    for name in uni:
+        u_str += name.lower() + "+"
 
-t_rows = soup.body.main.findAll('section', {'class': 'submissions'})[0].div.table.findAll('tr')
+    urls[uni[0]] = 'https://www.thegradcafe.com/survey/index.php?q=' + u_str + 'computer+science&t=a&o=&p=1'
 
-print(soup.body.main.table)
+responses = {}
+soups = {}
+t_rows = {}
+rows_id_dict = {}
 
-pass
+# Run Indefinitely
+while True:
+    for uni_key in urls.keys():
+        # Get response
+        responses[uni_key] = requests.get(urls[uni_key])
+
+        # Init Soup
+        soups[uni_key] = BeautifulSoup(responses[uni_key].text, "html.parser")
+
+        # Extract Table Rows
+        t_rows[uni_key] = \
+            soups[uni_key].body.main.findAll('section', {'class': 'submissions'})[0].div.table.findAll('tr')[1:]
+
+        # Extract Row IDs
+        u = []
+        for rr in t_rows[uni_key]:
+            u.append(
+                int(rr['onmouseover'].split(',')[1][:-2])
+            )
+
+        # Check if there is any new entry
+        if uni_key in rows_id_dict.keys():  # If key exists, means not first run
+            if rows_id_dict[uni_key][0] != u[0]:  # New entry found
+                # Find number of new entries
+                try:
+                    idx = u.index(rows_id_dict[uni_key][0])
+                    # Find if the newer ids were admits/rejects
+                    decisions = []
+                    no_r, no_a, no_o = 0, 0, 0
+                    for i in range(0, idx):
+                        d = str(t_rows[uni_key][i].findAll('td')[2].next).lower()
+                        if 'rejected' in d:
+                            no_r += 1
+                        elif 'accepted' in d:
+                            no_a += 1
+                        elif 'other' in d:
+                            no_o += 1
+                    print(no_r, no_a, no_o)
+
+                    message = str(uni_key) + "\n" + "Accept: " + str(no_a) + "\nReject: " + str(
+                        no_r) + "\nOther: " + str(no_o)
+                    notify.send(message)
+                    print(message)
+                    rows_id_dict[uni_key] = u
+                except Exception:
+                    print('Too many new indices')
+                    notify.send(str(uni_key) + "Too many new indices")
+
+            pass
+        else:
+            rows_id_dict[uni_key] = u[1:]
+        time.sleep(2)
+
+    time.sleep(10)
